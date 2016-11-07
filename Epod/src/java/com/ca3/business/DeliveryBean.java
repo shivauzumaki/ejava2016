ef8ac5faf8d3fac70a58e351aaf22dce94468f18
Vishnu S R <Vishnu S R>
2016-11-07 12:06:04 +0800

    Entity classess

--- Epod/src/java/com/ca3/business/DeliveryBean.java
+++ Epod/src/java/com/ca3/business/DeliveryBean.java
@@ -5,20 +5,29 @@
  */
 package com.ca3.business;
 
+import com.ca3.model.Delivery;
+import java.util.List;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
  * @author Vishnu
  */
+@Stateless
 public class DeliveryBean {
+    
+    private static final String getItems = "select d from Delivery d";
     
     @PersistenceContext private EntityManager em;
     
-    public void getItems(){
+    public List<Delivery> getItems(){
         
-        
+        TypedQuery<Delivery> deliveries = em.createQuery(getItems, Delivery.class);
+        System.out.println("Size "+ deliveries.getResultList().size());
+        return deliveries.getResultList();
     }
             
 }
--- Epod/src/java/com/ca3/rest/DeliveryItems.java
+++ Epod/src/java/com/ca3/rest/DeliveryItems.java
@@ -5,10 +5,18 @@
  */
 package com.ca3.rest;
 
+import com.ca3.business.DeliveryBean;
+import com.ca3.model.Delivery;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -18,12 +26,26 @@ import javax.ws.rs.Produces;
 @Path("/items")
 public class DeliveryItems {
     
+    @EJB private DeliveryBean deliveryBean;
+    
     @GET
     @Produces("appication/json")
     public Response getItems(){
         
-        
-        return
+        List<Delivery> deliveryList = deliveryBean.getItems();
+//        
+//        JsonObjectBuilder deliveryObject = Json.createObjectBuilder();
+//        JsonArrayBuilder arrayBuilder=Json.createArrayBuilder();
+//        deliveryObject.stream().forEach((app) -> {
+//            arrayBuilder.add(Json.createObjectBuilder()
+//                    .add("appointmentId",app.getAppt_id())
+//                    .add("dateTime", app.getAppt_date().getTime())
+//                    .add("description", app.getDescription())
+//                    .add("personId", app.getPeople().getPid())
+//                    .build()
+//            );
+//        });
+        return null;
     }
     
 }
